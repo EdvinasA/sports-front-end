f2cda1803c2f057b72cafc2b40cf37ba986ebf34
Edvinas <edvinasalimas98@gmail.com>
2022-11-05 16:57:20 +0200

    SPORT-1 Move code to formatter service

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { WorkoutDetails } from './models/workout';
+import { getDayOfTheMonth, getDayOfTheWeekForDate, getMonth } from './services/formatter-service';
 
-class App extends React.Component {
-  // private routes: [] = routes;
-  state = {
-    workout: [{
-      id: 1,
-      name: '',
-      bodyWeight: 1,
-      date: '',
-      startTime: '',
-      endTime: '',
-      notes: '',
-      exercises: [{
-        name: ''
-      }]
-    }]
+export interface WorkoutState {
+  workout: WorkoutDetails[];
+  loading: boolean,
+  error: boolean,
+}
+
+class App extends React.Component<{}, WorkoutState>  {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      workout: [],
+      loading: true,
+      error: false,
+    }
   }
 
   componentDidMount() {
@@ -36,73 +35,6 @@ class App extends React.Component {
       );
   }
 
-  getMonth(date: string) {
-    let fixed = new Date(date);
-    return this.getMonthName(fixed.getMonth()).slice(0, 5) + '.';
-  }
-
-  getDayOfTheWeekForDate(date: string) {
-    let fixed = new Date(date);
-    return this.getDayOfTheWeek(fixed.getDay()).slice(0, 3) + '.';
-  }
-
-  getDayOfTheMonth(date: string) {
-    let fixed = new Date(date);
-    return fixed.getDate() < 10 ? `0${fixed.getDate().toString()}` : fixed.getDate().toString();
-  }
-
-  getDayOfTheWeek(dayOfTheWeek: number) {
-    switch (dayOfTheWeek) {
-      case 1:
-        return 'Monday';
-      case 2:
-        return 'Tuesday';
-      case 3:
-        return 'Wednesday';
-      case 4:
-        return 'Thursday';
-      case 5:
-        return 'Friday';
-      case 6:
-        return 'Saturday';
-      case 7:
-        return 'Sunday';
-      default:
-        return "";
-    }
-  }
-
-  getMonthName(monthNumber: number) {
-    switch (monthNumber) {
-      case 1:
-        return "January";
-      case 2:
-        return "Feabruary";
-      case 3:
-        return "March";
-      case 4:
-        return "April";
-      case 5:
-        return "May";
-      case 6:
-        return "June";
-      case 7:
-        return "July";
-      case 8:
-        return "August";
-      case 9:
-        return "September";
-      case 10:
-        return "October";
-      case 11:
-        return "November";
-      case 12:
-        return "December";
-      default:
-        return "";
-    }
-  }
-
   render() {
     return (
       <div className="App">
@@ -111,9 +43,9 @@ class App extends React.Component {
             <div className="workout-list-wrapper" key={workout.id}>
               <div className="workout-item">
                 <div className="workout-item-column1">
-                  <div>{this.getDayOfTheWeekForDate(workout.date)}</div>
-                  <div>{this.getDayOfTheMonth(workout.date)}</div>
-                  <div>{this.getMonth(workout.date)}</div>
+                  <div>{getDayOfTheWeekForDate(workout.date)}</div>
+                  <div>{getDayOfTheMonth(workout.date)}</div>
+                  <div>{getMonth(workout.date)}</div>
                 </div>
                 <div className="workout-item-column2">{workout.name}</div>
               </div>
---
+++ app/src/services/formatter-service.ts
@@ -0,0 +1,66 @@
+export const getMonth = (date: string): string => {
+    let fixed = new Date(date);
+    return getMonthName(fixed.getMonth()).slice(0, 5) + '.';
+  }
+
+export const getDayOfTheWeekForDate = (date: string): string => {
+    let fixed = new Date(date);
+    return getDayOfTheWeek(fixed.getDay()).slice(0, 3) + '.';
+  }
+
+export const getDayOfTheMonth = (date: string) => {
+    let fixed = new Date(date);
+    return fixed.getDate() < 10 ? `0${fixed.getDate().toString()}` : fixed.getDate().toString();
+  }
+
+function getDayOfTheWeek(dayOfTheWeek: number) {
+    switch (dayOfTheWeek) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+      default:
+        return "";
+    }
+  }
+
+function getMonthName(monthNumber: number) {
+    switch (monthNumber) {
+      case 1:
+        return "January";
+      case 2:
+        return "Feabruary";
+      case 3:
+        return "March";
+      case 4:
+        return "April";
+      case 5:
+        return "May";
+      case 6:
+        return "June";
+      case 7:
+        return "July";
+      case 8:
+        return "August";
+      case 9:
+        return "September";
+      case 10:
+        return "October";
+      case 11:
+        return "November";
+      case 12:
+        return "December";
+      default:
+        return "";
+    }
+  }
