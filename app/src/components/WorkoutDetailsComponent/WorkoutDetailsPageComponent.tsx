8a19648629df462b8b9d7858ba80bf4f7bb1390d
Edvinas <edvinasalimas98@gmail.com>
2022-11-08 20:58:43 +0200

    SPORT-2 Add dislplay of exercises and sets

--- app/src/components/WorkoutDetailsComponent/WorkoutDetailsPageComponent.tsx
+++ app/src/components/WorkoutDetailsComponent/WorkoutDetailsPageComponent.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {WorkoutDetails} from '../../models/workout';
-import {Divider, TextField} from '@mui/material';
+import {Divider, IconButton, TextField} from '@mui/material';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment';
 import {TimePicker} from '@mui/x-date-pickers/TimePicker';
 import {MobileDatePicker} from '@mui/x-date-pickers/MobileDatePicker';
 import './WorkoutDetailsPageComponent.scss';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import HistoryIcon from '@mui/icons-material/History';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import StarIcon from '@mui/icons-material/Star';
 
 export interface WorkoutDetailsProps {
   workout: WorkoutDetails,
@@ -81,6 +85,61 @@ class WorkoutDetailsPage extends React.Component<WorkoutDetailsProps, WorkoutDet
             <TextField defaultValue={this.state.workout.notes || ''} label='Notes' variant="outlined"/>
           </div>
           <Divider/>
+          <div>
+            <div>
+              <div className='exercise'>
+                <div className='exercise-title'>Bench Press</div>
+                <div className='exercise-menu'>
+                  <IconButton>
+                    <MoreVertIcon/>
+                  </IconButton>
+                </div>
+              </div>
+              <div className='sets'>
+                <div>
+                  <div className='set-number'>1</div>
+                </div>
+                <div className='workout-weight-input'>
+                  <TextField defaultValue={this.state.workout.bodyWeight || ''} label='Weight' variant="outlined"/>
+                </div>
+                <div className='workout-weight-input'>
+                  <TextField defaultValue={this.state.workout.bodyWeight || ''} label='Reps' variant="outlined"/>
+                </div>
+                <div className='workout-weight-input'>
+                  <TextField defaultValue={this.state.workout.bodyWeight || ''} label='Notes' variant="outlined"/>
+                </div>
+                <div className='set-menu-icon'>
+                  <IconButton>
+                    <MoreVertIcon/>
+                  </IconButton>
+                </div>
+              </div>
+              <div className='actions'>
+                <div className='action-add-set'>
+                  <button>ADD SET</button>
+                </div>
+                <div>
+                  <IconButton>
+                    <HistoryIcon/>
+                  </IconButton>
+                </div>
+                <div>
+                  <IconButton>
+                    <BarChartIcon/>
+                  </IconButton>
+                </div>
+                <div>
+                  <IconButton>
+                    <StarIcon/>
+                  </IconButton>
+                </div>
+              </div>
+              <Divider/>
+            </div>
+            <div className='add-exercise-action'>
+              <button>+ ADD EXERCISE</button>
+            </div>
+          </div>
         </div>
     )
   }
