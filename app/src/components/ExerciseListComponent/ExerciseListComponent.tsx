bcf94715ed7fa5baf30478471fb2fa91052fa0c0
Edvinas <edvinasalimas98@gmail.com>
2022-11-23 20:00:43 +0200

    SPORT-7 Add all components for display exercise list

--- app/src/components/ExerciseListComponent/ExerciseListComponent.tsx
+++ app/src/components/ExerciseListComponent/ExerciseListComponent.tsx
@@ -1,7 +1,8 @@
 import './ExerciseListComponent.scss';
 import React, {useEffect} from "react";
 import {getAllExercises} from "../../services/ExerciseService";
-import {Exercise, WorkoutExercise} from "../../models/workout";
+import {Exercise} from "../../models/workout";
+import {ArrowBack, Add, MoreVert} from "@mui/icons-material";
 
 const ExerciseListComponent = () => {
   const [exercises, setExercises] = React.useState([]);
@@ -14,14 +15,34 @@ const ExerciseListComponent = () => {
 
   return (
       <div className="ExerciseListComponent">
+        <div className='exercise-edit-list-header'>
+          <div className='exercise-edit-list-column1'>
+            <div>
+              <ArrowBack/>
+            </div>
+            <div>
+              Edit Exercises
+            </div>
+          </div>
+          <div className='exercise-edit-list-column1'>
+            <Add/>
+          </div>
+        </div>
         <div>
           {exercises &&
               exercises.map((exercise: Exercise) => (
-                  <div>{exercise.name}</div>
+                  <div className='exercise-edit-list-display'>
+                    <div>
+                      {exercise.name}
+                    </div>
+                    <div>
+                      <MoreVert/>
+                    </div>
+                  </div>
               ))}
         </div>
       </div>
   )
 };
 
-export { ExerciseListComponent };
+export {ExerciseListComponent};
